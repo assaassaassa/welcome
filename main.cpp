fe9d2ddaee2b6d501c1e7ff39fe9d6978cd3d74e
assaassaassa <34476753+assaassaassa@users.noreply.github.com>
2017-12-12 17:23:31 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main (int argc, char *argv[])
 {
 	auto logger = spdlog::stdout_logger_mt("console");
-	logger->info("version {} was started", version());
+	logger->info("HelloWorld version {} was started", version());
 	
 	return 0;
 }
